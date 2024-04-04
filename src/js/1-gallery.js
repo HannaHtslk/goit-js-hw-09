051b57ffdd165136fccd2871f35175f152025e59
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-04 14:13:41 +0300

    upd

---
+++ src/js/1-gallery.js
@@ -0,0 +1,12 @@
+
+
+import simpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+console.log(simpleLightbox)
