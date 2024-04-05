17dfbdbf563cfa9b5bb7712649c9308a0cc36ca4
AnnaHtslk <hannah.htslk@gmail.com>
2024-04-05 17:09:09 +0300

    form done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,46 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const userData = {};
+
+
+const fillInForm = () => {
+    try {
+        const savedData = JSON.parse(localStorage.getItem('feedback-form-state')); 
+        
+        if (savedData === null) {
+            return;
+        }
+        console.log(savedData);
+
+        for (const key in savedData) {
+            form.elements[key].value = savedData[key];
+        }
+        
+  
+    } catch (error) {
+        console.log(error);
+    }
+}
+fillInForm();
+
+
+const onFormInput = event => {
+    const { target: clickedElement } = event;
+    
+    const clickedElementName = clickedElement.name;
+    const clickedElementValue = clickedElement.value;
+   
+    userData[clickedElementName] = clickedElementValue;
+    console.log(userData)
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+const onFormSubmit = event => {
+    event.preventDefault();
+
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+}
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
